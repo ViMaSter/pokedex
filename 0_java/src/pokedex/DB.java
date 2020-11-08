8b7dd90a29f6d393f5096c9afcf461f0d65721bf
Vincent Mahnke <vincent@mahn.ke>
2020-11-08 02:56:43 +0100

    Add support for DB connections based on config file

--- 0_java/src/pokedex/DB.java
+++ 0_java/src/pokedex/DB.java
@@ -1,17 +1,37 @@
 package pokedex;
 
 import java.sql.Connection;
+import java.io.*;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class DB {
-	public static ArrayList<Pokemon> GetAllPokemon() throws SQLException
-	{
+import sun.security.util.IOUtils;
+
+public class DB {		
+	private static String readString(InputStream inputStream) throws IOException {
+
+	    ByteArrayOutputStream into = new ByteArrayOutputStream();
+	    byte[] buf = new byte[4096];
+	    for (int n; 0 < (n = inputStream.read(buf));) {
+	        into.write(buf, 0, n);
+	    }
+	    into.close();
+	    return new String(into.toByteArray(), "UTF-8");
+	}
+	
+	private static String GetConnectionString() throws ClassNotFoundException, IOException
+	{		
+        Class.forName("com.mysql.jdbc.Driver"); 
+		return readString(DB.class.getClassLoader().getResourceAsStream("/pokedex/config/remotedb.txt"));
+	}
+	
+	public static ArrayList<Pokemon> GetAllPokemon() throws SQLException, ClassNotFoundException, IOException
+	{ 
 		ArrayList<Pokemon> Pokemon = new ArrayList<Pokemon>();
-	    Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=localDB;user=sa;password=Od1P_DMu_LF2qw3UtGzwX1fxJCv-cB3N");
+	    Connection con = DriverManager.getConnection(GetConnectionString());
 
 	    PreparedStatement statement = con.prepareStatement("SELECT * from Pokemon");
 		ResultSet set = statement.executeQuery();
@@ -21,9 +41,9 @@ public class DB {
 		}
 		return Pokemon;
 	}
-	public static Pokemon GetPokemonByID(int ID) throws SQLException
-	{
-	    Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=localDB;user=sa;password=Od1P_DMu_LF2qw3UtGzwX1fxJCv-cB3N");
+	public static Pokemon GetPokemonByID(int ID) throws SQLException, ClassNotFoundException, IOException
+	{ 
+	    Connection con = DriverManager.getConnection(GetConnectionString());
 
 	    PreparedStatement statement = con.prepareStatement("SELECT * from Pokemon WHERE ID = ?");
 	    statement.setInt(1, ID);
@@ -36,10 +56,10 @@ public class DB {
 		return new Pokemon(set.getInt("id"), set.getString("name"));
 	}
 	
-	public static ArrayList<Pokemon> GetEvolutionsByID(int ID) throws SQLException
+	public static ArrayList<Pokemon> GetEvolutionsByID(int ID) throws SQLException, ClassNotFoundException, IOException
 	{
 		ArrayList<Pokemon> Pokemon = new ArrayList<Pokemon>();
-	    Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=localDB;user=sa;password=Od1P_DMu_LF2qw3UtGzwX1fxJCv-cB3N");
+	    Connection con = DriverManager.getConnection(GetConnectionString());
 
 	    PreparedStatement statement = con.prepareStatement("SELECT Pokemon.* FROM Evolutions INNER JOIN Pokemon ON Pokemon.ID = Evolutions.EvolutionID WHERE Evolutions.ID = ? ");
 	    statement.setInt(1, ID);
